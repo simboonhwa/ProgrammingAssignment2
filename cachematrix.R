edcf0b1354959c1f24ce1a5c9f3c295854c770a7
Sim Boon Hwa <simboonhwa@localhost.localdomain>
2015-02-23 00:13:02 +0800

    update with lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,22 @@
 ##
 ## ARGUMENT:	Matrix object
 ##
-## METHOD: 	set, get (to set or return the matrix) 
-##		setinv, getinv (to set or return the inverse matrix) 
-## ATTRIBUTE: 	m (to store value of the inverse matrix)
+## METHOD: 	set, get (to set or return the matrix or x) 
+##		setinv, getinv (to set or return the INVERSE matrix or m) 
+##
+## ATTRIBUTE: 	x (to store the value of matrix)
+##		m (to store value of the inverse matrix)
 ##
 ## eg. 	matrixobject <- makeCacheMatrix(mymatrix)
 ##      ^ instance	^ class
 ##
 ##	matrixobject$get()
 ##		     ^ method
+##
+## Lexical scope: only "set" & "setinv" functions need to use "<<-"
+##	to refer to attribute x(matrix)& m(inv matrix) within 
+##	the instance (matrixobject). Without "<<-", it will complain object not
+##	found during runtime.
 ###################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -57,3 +64,21 @@ cacheSolve <- function(x, ...) {
         x$setinv(m)
         m
 }
+
+##################################################################
+##
+## Example: 	
+##	B = matrix(c(2, 4, 3, 1,),nrow=2,ncol=2) 	
+## 	** create mymatrix object with B (x=B, m=NULL) 	
+## 	mymatrix <- makeCacheMatrix(B) 
+##	** to find inv of B, execute cacheSolve (x=B, m=(inv)B)
+##	myinvmatrix <- cacheSolve(mymatrix)
+##	** if excute cacheSolve(mymatrix) again, it will return m which already
+##	** store the (inv)B in m
+##
+## 	** create mymatrix object with empty matrix (x=NULL, m=NULL)	
+## 	mymatrix <- makeCacheMatrix() (x=NULL, m=NULL)
+## 	** To assign mymatrix object with B matrix (x=B, m=NULL)	
+## 	mymatrix.set(B) 
+##################################################################
+
